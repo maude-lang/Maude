95e37e076e6dbb4a1c4f1adf134a19c16b86fcf0
Steven Eker <steveneker@gmail.com>
2025-01-11 21:41:32 -0800

    started function pointer eqRewrite implmentation

--- src/Interface/symbol.hh
+++ src/Interface/symbol.hh
@@ -2,7 +2,7 @@
 
     This file is part of the Maude 3 interpreter.
 
-    Copyright 1997-2024 SRI International, Menlo Park, CA 94025, USA.
+    Copyright 1997-2025 SRI International, Menlo Park, CA 94025, USA.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -37,9 +37,11 @@
 #include "ruleTable.hh"
 #include "strategy.hh"
 #include "memoTable.hh"
+#include "eqRewriter.hh"
 
 class Symbol
   : public RuleTable,
+    public EqRewriter,
     public NamedEntity,
     public LineNumber,
     public SortTable,
