46d4aebe9485bb92daa1035cb7c4cb459e519412
Rubén Rubio <rubenrub@ucm.es>
2025-09-26 11:18:05 +0200

    Added CVC5 support (fixes #7)

--- src/Mixfix/cvc5_Bindings.hh
+++ src/Mixfix/cvc5_Bindings.hh
@@ -2,7 +2,8 @@
 
     This file is part of the Maude 3 interpreter.
 
-    Copyright 1997-2014 SRI International, Menlo Park, CA 94025, USA.
+    Copyright 1997-2017 SRI International, Menlo Park, CA 94025, USA.
+    Copyright 2025 Universidad Complutense de Madrid, 28040 Madrid, Spain.
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -21,24 +22,13 @@
 */
 
 //
-//      Class for generating SMT variables, with dummy version for no SMT support.
+//      Class for generating SMT variables, version for CVC5 support.
 //
-#ifndef _variableGenerator_hh_
-#define _variableGenerator_hh_
-#include "SMT_Info.hh"
-#include "SMT_EngineWrapper.hh"
+#ifndef _cvc5_Bindings_hh_
+#define _cvc5_Bindings_hh_
 
-#ifdef USE_CVC4
-#include "cvc4_Bindings.hh"
-#elif defined(USE_CVC5)
-#include "cvc5_Bindings.hh"
-#elif defined(USE_YICES2) 
-#include "yices2_Bindings.hh"
-#else
-
-//
-//	Code for no SMT support case.
-//
+#include <map>
+#include "cvc5/cvc5.h"
 
 class VariableGenerator : public SMT_EngineWrapper
 {
@@ -55,7 +45,33 @@ public:
   void pop();
 
   VariableDagNode* makeFreshVariable(Term* baseVariable, const mpz_class& number);
+
+private:
+  //
+  //	We identify Maude variables that correspond to SMT variables by a pair
+  //	where the first component in the variable's sort's index within its module
+  //	and the second component is the variables name.
+  //
+  typedef pair<int, int> SortIndexVariableNamePair;
+  //
+  //	When we generate SMT variables on-the-fly we keep track of them in a map.
+  //
+  typedef map<SortIndexVariableNamePair, cvc5::Term> VariableMap;
+
+  cvc5::Term makeVariable(VariableDagNode* v);
+  cvc5::Term makeBooleanExpr(DagNode* dag);
+  cvc5::Term makeNumberConstant(const mpq_class& rational, bool isInteger);
+  cvc5::Term dagToCvc5(DagNode* dag);
+
+  const SMT_Info& smtInfo;
+
+  VariableMap variableMap;
+  int pushCount;
+  //
+  //	CVC5 objects.
+  //
+  cvc5::TermManager termManager;
+  cvc5::Solver smtSolver;
 };
 
-#endif
 #endif
